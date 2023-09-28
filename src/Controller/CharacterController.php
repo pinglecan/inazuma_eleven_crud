b98a70f88b788a26031dd76e0eaf5039245d41c6
pinglecan <99071834@mydavinci.nl>
2023-09-28 11:06:18 +0200

    .

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request; 
 use App\Entity\Character;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class CharacterController extends AbstractController
 {
@@ -20,7 +21,6 @@ class CharacterController extends AbstractController
     private $characterRepository;
     public function __construct(CharacterRepository $characterRepository, EntityManagerInterface $em) 
     {
-        // $this->raceRepository = $raceRepository;
         $this->characterRepository = $characterRepository;
         $this->em = $em;
     }
@@ -36,19 +36,104 @@ class CharacterController extends AbstractController
     }
 
     #[Route("/characters/create", name:"create_movie")]
-    public function create(): Response
+    public function create(Request $request): Response
     {
         $character = new Character();
         $form = $this->createForm(CharacterFormType::class, $character);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form -> isValid()) {
+            $newCharacter = $form->getData();
+
+            $image = $form->get('image')->getData();
+            if($image) {
+                $newFileName = uniqid() . '.' . $image->guessExtension();
+
+                try{
+                    $image->move(
+                        $this->getParameter('kernel.project_dir') . '/public/uploads',
+                        $newFileName
+                    );
+                } catch (FileExeption $e) {
+                    return new response($e ->getMessage());
+                }
+
+                $newCharacter ->setimage('/uploads/' . $newFileName);
+            }
+            
+            $this->em->persist($newCharacter);
+            $this->em->flush();
+
+            return $this ->redirectToroute('characters');
+
+        }
+        
+        if ($form->isSubmitted() && $form->isValid()) { 
+            
+        }
 
         return $this->render('characters/create.html.twig',[
             'form' => $form->createView()
         ]);
     }
 
-    #[Route('/characters/{id}', methods: ['GET'], name: 'character')]
+
+    #[Route('/characters/edit/{id}', name:'edit_character')]
+    public function edit($id, Request $request): Response
+    {
+        $character = $this->characterRepository->find($id);
+        $form = $this->createForm(CharacterFormType::class, $character);
+        $form->handleRequest($request);
+        $image = $form-> get('image')->getData();    
+
+        if ($form->isSubmitted() && $form->isValid()){
+            if ($image) { 
+                if($character->getImage() !== null){
+                    if(file_exists(
+                        $this->getParameter('kernel.project_dir') . '/public' . $character->getImage()
+                    )){
+
+                        $this->getParameter('kernel.project_dir') . '/public' . $character->getImage();
+                        $newFileName = uniqid() . '.' . $image->guessExtension();
+
+                        try{
+                            $image->move(
+                                $this->getParameter('kernel.project_dir') . '/public/uploads',
+                                $newFileName
+                            );
+                        } catch (FileExeption $e) {
+                            return new response($e ->getMessage());
+                        }
+
+                        $character->setimage('/uploads/' . $newFileName);
+                        $this->em->flush();
+
+                        return $this->redirectToRoute('characters');
+                    }
+                }
+            }else{
+                    
+                    $character->SetTitle($form->get('title')->getData());
+                    $character->setgender($form->get('gender')->getData());
+                    $character->setPosision($form->get('posision')->getdata());
+            
+                    $this->em->flush();
+                    return $this->redirectToRoute('characters');
+                }      
+
+        }
+
+        return $this->render('characters/edit.html.twig',[
+            'character' => $character,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    #[Route("/characters/{id}", methods: ['GET'], name: 'character')]
     public function show($id): Response
     {
+    
         $character = $this->characterRepository->find($id);
 
         return $this->render('characters/show.html.twig', [
--- src/Form/CharacterFormType.php
+++ src/Form/CharacterFormType.php
@@ -6,16 +6,53 @@ use App\Entity\Character;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class CharacterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('image')
-            ->add('gender')
-            ->add('posision')
+            ->add('title', TextType::class,[
+                'attr' => array(
+                    'class' => 'bg-tranparent block border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter Title...'
+                ),
+                'label' => false
+            ])
+
+
+            ->add('posision', ChoiceType::class,[
+                'attr' => array(
+                    'class' => 'bg-tranparent block border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter posision...'
+                ),
+                'choices'  => [
+                    'forward (FW)' => 'FW',
+                    'midfielder (MF)' => 'MF',
+                    'defender (DF)' => 'DF',
+                    'goalkeeper (GK)' => 'GK'
+                ],
+            ])
+            ->add('gender', ChoiceType::class,[
+                'attr' => array(
+                    'class' => 'bg-tranparent block border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter gender...'
+                ),
+                'choices'  => [
+                    'male' => 'male',
+                    'female' => 'female',
+                    'femboy'=> 'femboy'
+                ],
+            ])
+
+            
+            ->add('image', FileType::class, array(
+                'required' => false,
+                'mapped' => false
+            ))
             //->add('teams')
         ;
     }
